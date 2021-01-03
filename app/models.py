016ae5c4a4e602f835d20a72c0239d616f9de991
00MB <s2061204@ed.ac.uk>
2021-01-03 23:28:52 +0000

    Updated fonts

--- app/models.py
+++ app/models.py
@@ -1,9 +1,16 @@
 from app import db
-from random import randint
+import uuid
+
+def key_generator():
+    key = str(uuid.uuid4()) # Convert UUID format to a Python string.
+    key = key.upper()
+    key = key.replace("-","")
+    return key[0:6]
 
 class Timeline(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     eventname = db.Column(db.String(64), index=True)
+    eventkey = db.Column(db.String(6), index=True, unique=True)
 
     def __repr__(self):
-        return '<Timeline {}>.format(self.eventname)'
+        return f"name: {self.eventname}, key: {self.eventkey}"
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,29 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for, make_response, request
 from app import app, db
 from app.forms import Newevent
-from app.models import Timeline
+from app.models import Timeline, key_generator
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello"
 
+@app.route('/timeline')
+def timeline():
+    print(request.cookies.get('eventkey'))
+    return render_template('timeline.html')
+
 @app.route('/newevent', methods=['GET', 'POST'])
 def newevent():
     form = Newevent()
     print(form.validate_on_submit())
     if form.validate_on_submit():
-        event = Timeline(eventname = form.eventname.data)
+        event = Timeline(eventname = form.eventname.data, eventkey = key_generator())
+        print(event)
+        res = make_response(redirect(url_for('timeline')))
+        res.set_cookie('eventname', event.eventname)
+        res.set_cookie('eventkey', event.eventkey)
         db.session.add(event)
         db.session.commit()
-        return redirect('/index')
+        return res
     return render_template('create-timeline.html', title="test", form=form)
